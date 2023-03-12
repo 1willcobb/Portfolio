9c84f833850f9f8931391d0f8c74e5976eec3ad5
1willcobb <cobb.will@gmail.com>
2023-03-12 11:29:12 -0700

    fixing portfolio

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const sidebarToggle = document.querySelector('.side-nav-selector');
-const sidebar = document.querySelector('.side-nav');
-const hidden = document.querySelector('.hidden');
-var r = document.querySelector(':root');
+const sidebarToggle = document.querySelector(".side-nav-selector");
+const sidebar = document.querySelector(".side-nav");
+const hidden = document.querySelector(".hidden");
+var r = document.querySelector(":root");
 let sideBarEnabled = false;
 
+// TODO: Shift
 function sideBarShift() {
-  r.style.setProperty('--side-bar-size', '20rem');
-};
+  r.style.setProperty("--side-bar-size", "20rem");
+}
 
 function sideBarReset() {
-  r.style.setProperty('--side-bar-size', '6rem');
-};
+  r.style.setProperty("--side-bar-size", "6rem");
+}
 
-sidebarToggle.addEventListener('click', () => {
+sidebarToggle.addEventListener("click", () => {
   sideBarEnabled = !sideBarEnabled;
   if (sideBarEnabled) {
     sideBarShift();
-    hidden.classList.toggle('show');
+    hidden.classList.toggle("show");
   } else {
     sideBarReset();
-    hidden.classList.toggle('show');
+    hidden.classList.toggle("show");
   }
 });
